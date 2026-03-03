b11c944b9125d685f670082f6480ce61c99bcfee
Tuhin Reza <tuhinreza02042002@gmail.com>
2026-03-04 00:25:52 +0600

    Learn vocubulary section done

--- vocabulary.js
+++ vocabulary.js
@@ -10,11 +10,6 @@ const displayLessions = (lessons) => {
   const level = document.getElementById("level-container");
   level.innerHTML = "";
 
-  if (!lessons || lessons.length === 0) {
-    level.innerHTML = "<p>No lessons found!</p>";
-    return;
-  }
-
   for (const lesson of lessons) {
     const btnDiv = document.createElement("div");
 
@@ -37,11 +32,52 @@ const loadLevelWord = (id) => {
     .then((res) => res.json())
     .then((data) => displayLevelWOrd(data.data));
 };
-const displayLevelWOrd=(element)=>{
-    const word = document.getElementById("word-contarinar");
-     word.innerHTML='';
-     const newWord = `
-     
+const displayLevelWOrd = (element) => {
+  const word = document.getElementById("word-container");
+  word.innerHTML = "";
+  if (element.length == 0) {
+    const newElement = `
+           <div class="col-span-3 flex justify-center items-center flex-col gap-5">
+        <div class="text-center">
+         <img src="./english-janala-resources/assets/alert-error.png" alt="">
+        </div>
+
+        <p class="bangla font-semibold text-2xl">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-3xl bangla">নেক্সট Lesson এ যান</h2>
+      </div>
+       `;
+      word.innerHTML=newElement;
+
+     return;
+  }
+
+  element.forEach((element) => {
+    const newWord = `
+      <div class="flex flex-col gap-4 justify-center items-center bg-white p-8 rounded-2xl shadow-sm hover:shadow-md transition-shadow duration-300 border border-gray-100">
+    
+    <h2 class="text-2xl font-bold text-blue-600 tracking-wide uppercase ">${element.word}</h2>
+    
+    <div class="text-center">
+      <p class="text-xs text-gray-400 uppercase tracking-widest mb-1">${element.meaning}</p>
+      <h1 class="text-xl font-semibold text-gray-800">${element.pronunciation}</h1>
+    </div>
+
+    <div class="flex items-center justify-between w-full mt-4 px-4 border-t pt-4 text-gray-500">
+      <button class="hover:text-blue-500 transition-colors">
+        <i class="fa-solid fa-circle-info text-xl"></i>
+      </button>
+      <button class="hover:text-blue-500 transition-colors">
+        <i class="fa-solid fa-volume-low text-xl"></i>
+      </button>
+    </div>
+
+  </div>
+
+
      `;
-}
+   word.innerHTML+=newWord;
+    
+  });
+  // console.log(word);
+};
 loadLessions();
